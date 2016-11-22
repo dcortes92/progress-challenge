b6d130a6d4d2d97ac86302cb976f14d34d4f9e50
Daniel Cortes Saenz <dcortes92@hotmail.com>
2016-11-22 12:13:55 -0600

    feat(Dynamic Progress): Made the progress bar dynamic

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,8 +1,27 @@
 $(function() {
+
+  var $modal = $('#myProgressModal');
+  // change this two values to adjust the progress
+  var reached = 56;
+  var target = 125;
+  var remaining = target - reached;
+  var percentage = (reached * 100) / target;
+
   var $progress,
       $progressWidth,
       $progressSpan;
-  $('#myProgressModal').on('show.bs.modal', function() {
+
+  // set initial state
+  $modal.find('.progress-bar .current').html('$' + reached)
+  $modal.find('.progress-bar').css({
+    width: percentage + '%'
+  });
+  $modal.find('.remaining').html('$' + remaining);
+  $modal.find('.target').html('Target $' + target);
+
+  // set modal state
+
+  $modal.on('show.bs.modal', function() {
     $progress = $('.progress .progress-bar');
     $progressWidth = $progress.width();
     $progressSpan = $progress.find('span');
@@ -11,7 +30,7 @@ $(function() {
     $progressSpan.fadeOut(0);
   });
 
-  $('#myProgressModal').on('shown.bs.modal', function() {
+  $modal.on('shown.bs.modal', function() {
     $progress.animate({
       width: $progressWidth + '%'
     }, 200);
